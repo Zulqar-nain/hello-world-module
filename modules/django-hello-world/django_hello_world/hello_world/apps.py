cf47287a1f64b120a4cd3d4398587632374d424d
Zulqarnain <zulqarnain.haider@tkxel.com>
2024-04-05 13:12:40 +0500

    first commit

---
+++ modules/django-hello-world/django_hello_world/hello_world/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HelloWorldConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'modules.django_hello_world.hello_world'
